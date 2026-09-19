329055f0c872073e0363eb25a47aa0896f20b73a
Андрей Нечаев <kefir4ik210@gmail.com>
2026-09-19 17:39:13 +0300

    Добавил кнопки на главный экран

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.cutiepiaki.retrolog"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.cutiepiaki.retrolog"
         minSdk = 31
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -38,8 +38,11 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -47,7 +50,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/cutiepiaki/retrolog/MainActivity.kt
+++ app/src/main/java/com/cutiepiaki/retrolog/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import com.cutiepiaki.retrolog.ui.theme.RetroLogTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +31,250 @@ class MainActivity : ComponentActivity() {
         setContent {
             RetroLogTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Платформы",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+
+                            Row {
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(1f)
+                                                .fillMaxWidth(),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("GB")
+                                        }
+                                    }
+                                }
+
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier.fillMaxSize() // ← важно: Column должен занять всю кнопку
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+                                        Box(
+                                            modifier = Modifier.weight(1f),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("GBC")
+                                        }
+                                    }
+                                }
+
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier.fillMaxSize() // ← важно: Column должен занять всю кнопку
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+                                        Box(
+                                            modifier = Modifier.weight(1f),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("GBA")
+                                        }
+                                    }
+                                }
+                            }
+
+                            Row(
+                                modifier = Modifier.padding(vertical = 10.dp)
+                            ) {
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier.fillMaxSize() // ← важно: Column должен занять всю кнопку
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+                                        Box(
+                                            modifier = Modifier.weight(1f),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("NES")
+                                        }
+                                    }
+                                }
+
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier.fillMaxSize() // ← важно: Column должен занять всю кнопку
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+                                        Box(
+                                            modifier = Modifier.weight(1f),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("SNES")
+                                        }
+                                    }
+                                }
+
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier.fillMaxSize() // ← важно: Column должен занять всю кнопку
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+                                        Box(
+                                            modifier = Modifier.weight(1f),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("Sega MD")
+                                        }
+                                    }
+                                }
+                            }
+                            Row(
+                                modifier = Modifier.padding(vertical = 10.dp)
+                            ) {
+                                Button(
+                                    shape = RectangleShape,
+                                    onClick = { },
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp)
+                                        .width(130.dp)
+                                        .height(150.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier.fillMaxSize()
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .weight(5f)
+                                                .fillMaxWidth()
+                                                .background(Color.Magenta),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text("*IMAGE*")
+                                        }
+                                        Box(
+                                            modifier = Modifier.weight(1f),
+                                            contentAlignment = Alignment.BottomCenter
+                                        ) {
+                                            Text("Sony PS1")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RetroLogTheme {
-        Greeting("Android")
     }
 }
